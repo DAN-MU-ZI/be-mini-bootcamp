238c526a440f64f4b1d9e2f7476db7eea19727dd
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-07-30 14:15:32 +0900

    impl answer feature

---
+++ src/main/java/com/study/bootcamp/answer/AnswerService.java
@@ -0,0 +1,21 @@
+package com.study.bootcamp.answer;
+
+import com.study.bootcamp.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+    }
+}
