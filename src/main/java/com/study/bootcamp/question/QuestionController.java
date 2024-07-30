0bb407a22f05b35b027373eecef789af6d54e290
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-07-30 13:19:57 +0900

    root url redirection

--- src/main/java/com/study/bootcamp/question/QuestionController.java
+++ src/main/java/com/study/bootcamp/question/QuestionController.java
@@ -1,15 +1,21 @@
 package com.study.bootcamp.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        model.addAttribute("abc", "13");
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
 
         return "question_list";
     }
