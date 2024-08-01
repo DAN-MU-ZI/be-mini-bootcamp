f59eef1a44d2f7e5f40d362b6677528a69829158
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-08-01 14:01:22 +0900

    impl anchor

--- src/main/java/com/study/bootcamp/answer/AnswerService.java
+++ src/main/java/com/study/bootcamp/answer/AnswerService.java
@@ -13,13 +13,13 @@ import java.time.LocalDateTime;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
-        answerRepository.save(answer);
+        return answerRepository.save(answer);
     }
 
     public Answer getAnswer(Integer id) {
